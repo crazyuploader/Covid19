7712ac6622f214939fbc27ab3ea829e4542177e9
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-04-28 12:40:25 +0530

    Update Covid 19
    Covid19/.idea/gradle.xml
    Covid19/app/src/main/java/com/example/covid19/MainActivity.java
    Covid19/app/src/main/res/drawable/roundedbutton.xml
    Covid19/app/src/main/res/layout/activity_main.xml
    Covid19/app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/covid19/MainActivity.java
+++ app/src/main/java/com/example/covid19/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.covid19;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,7 +20,9 @@ public class MainActivity extends AppCompatActivity {
 
     Button buttonFetch;
     TextView tvFetched;
-    String country;
+    String baseURL = "https://corona.lmao.ninja/v2/countries/";
+    String userInput, country;
+    EditText editInput;
     int cases, todayCases, deaths, todayDeaths, recovered;
 
     @Override
@@ -29,43 +32,46 @@ public class MainActivity extends AppCompatActivity {
 
         buttonFetch = findViewById(R.id.buttonFetch);
         tvFetched = findViewById(R.id.tvFetched);
-
-        String url = "https://corona.lmao.ninja/v2/countries/india";
+        editInput = findViewById(R.id.editInput);
 
         final RequestQueue requestQueue = Volley.newRequestQueue(this);
-
-        final StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject reader = new JSONObject(response);
-                            country = reader.getString("country");
-                            cases = reader.getInt("cases");
-                            todayCases = reader.getInt("todayCases");
-                            deaths = reader.getInt("deaths");
-                            todayDeaths = reader.getInt("todayDeaths");
-                            recovered = reader.getInt("recovered");
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        tvFetched.setText("Country: " + country + "\n"
-                                + "Total Cases: " + cases + "\n"
-                                + "Today Cases: " + todayCases + "\n"
-                                + "Total Deaths: " + deaths + "\n"
-                                + "Today Deaths: " + todayDeaths + "\n"
-                                + "Recovered: " + recovered);
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                tvFetched.setText("That didn't work!");
-            }
-        });
 
                 buttonFetch.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+
+                        String Input = editInput.getText().toString();
+
+                        String url = baseURL + Input;
+
+                        final StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                                new Response.Listener<String>() {
+                                    @Override
+                                    public void onResponse(String response) {
+                                        try {
+                                            JSONObject reader = new JSONObject(response);
+                                            country = reader.getString("country");
+                                            cases = reader.getInt("cases");
+                                            todayCases = reader.getInt("todayCases");
+                                            deaths = reader.getInt("deaths");
+                                            todayDeaths = reader.getInt("todayDeaths");
+                                            recovered = reader.getInt("recovered");
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                        tvFetched.setText("Country: " + country + "\n"
+                                                + "Total Cases: " + cases + "\n"
+                                                + "Today Cases: " + todayCases + "\n"
+                                                + "Total Deaths: " + deaths + "\n"
+                                                + "Today Deaths: " + todayDeaths + "\n"
+                                                + "Recovered: " + recovered);
+                                    }
+                                }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                tvFetched.setText("That didn't work!");
+                            }
+                        });
                         requestQueue.add(stringRequest);
                     }
                 });
