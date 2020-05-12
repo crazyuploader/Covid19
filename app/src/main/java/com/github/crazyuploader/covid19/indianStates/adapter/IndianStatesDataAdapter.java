818d6e9c8b45d292e34b67cd350a49cfb2f438d4
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-05-12 21:55:34 +0530

    Update

--- app/src/main/java/com/github/crazyuploader/covid19/indianStates/adapter/IndianStatesDataAdapter.java
+++ app/src/main/java/com/github/crazyuploader/covid19/indianStates/adapter/IndianStatesDataAdapter.java
@@ -1,5 +1,39 @@
 package com.github.crazyuploader.covid19.indianStates.adapter;
 
-public class IndianStatesDataAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.github.crazyuploader.covid19.indianStates.IndianStatesData;
+
+public class IndianStatesDataAdapter extends RecyclerView.Adapter<IndianStatesDataAdapter.IndianStatesViewHolder> {
+
+    private final IndianStatesData[] fetched;
+
+    public IndianStatesDataAdapter(IndianStatesData[] fetched) {
+        this.fetched = fetched;
+    }
+
+    @NonNull
+    @Override
+    public IndianStatesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull IndianStatesViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return fetched.length;
+    }
+
+    public class IndianStatesViewHolder extends RecyclerView.ViewHolder {
+        public IndianStatesViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 
 }
