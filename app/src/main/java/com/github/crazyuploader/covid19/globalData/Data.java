5ff2a178c81bcfacf247a6229e558d6e793be26f
Restyled.io <commits@restyled.io>
2020-05-12 17:42:13 +0000

    Restyled by clang-format

--- app/src/main/java/com/github/crazyuploader/covid19/globalData/Data.java
+++ app/src/main/java/com/github/crazyuploader/covid19/globalData/Data.java
@@ -6,110 +6,55 @@ import com.google.gson.annotations.SerializedName;
 
 public class Data {
 
-    @SerializedName("updated")
-    @Expose
-    private Long updated;
-    @SerializedName("country")
-    @Expose
-    private String country;
-    @SerializedName("countryInfo")
-    @Expose
-    private CountryInfo countryInfo;
-    @SerializedName("cases")
-    @Expose
-    private Integer cases;
-    @SerializedName("todayCases")
-    @Expose
-    private Integer todayCases;
-    @SerializedName("deaths")
-    @Expose
-    private Integer deaths;
-    @SerializedName("todayDeaths")
-    @Expose
-    private Integer todayDeaths;
-    @SerializedName("recovered")
-    @Expose
-    private Integer recovered;
-    @SerializedName("active")
-    @Expose
-    private Integer active;
-    @SerializedName("critical")
-    @Expose
-    private Integer critical;
-    @SerializedName("casesPerOneMillion")
-    @Expose
-    private Integer casesPerOneMillion;
-    @SerializedName("deathsPerOneMillion")
-    @Expose
-    private Integer deathsPerOneMillion;
-    @SerializedName("tests")
-    @Expose
-    private Integer tests;
-    @SerializedName("testsPerOneMillion")
-    @Expose
-    private Integer testsPerOneMillion;
-    @SerializedName("continent")
-    @Expose
-    private String continent;
+  @SerializedName("updated") @Expose private Long updated;
+  @SerializedName("country") @Expose private String country;
+  @SerializedName("countryInfo") @Expose private CountryInfo countryInfo;
+  @SerializedName("cases") @Expose private Integer cases;
+  @SerializedName("todayCases") @Expose private Integer todayCases;
+  @SerializedName("deaths") @Expose private Integer deaths;
+  @SerializedName("todayDeaths") @Expose private Integer todayDeaths;
+  @SerializedName("recovered") @Expose private Integer recovered;
+  @SerializedName("active") @Expose private Integer active;
+  @SerializedName("critical") @Expose private Integer critical;
+  @SerializedName("casesPerOneMillion")
+  @Expose
+  private Integer casesPerOneMillion;
+  @SerializedName("deathsPerOneMillion")
+  @Expose
+  private Integer deathsPerOneMillion;
+  @SerializedName("tests") @Expose private Integer tests;
+  @SerializedName("testsPerOneMillion")
+  @Expose
+  private Integer testsPerOneMillion;
+  @SerializedName("continent") @Expose private String continent;
 
-    public long getUpdated() {
-        return updated;
-    }
+  public long getUpdated() { return updated; }
 
-    public String getCountry() {
-        return country;
-    }
+  public String getCountry() { return country; }
 
-    public CountryInfo getCountryInfo() {
-        return countryInfo;
-    }
+  public CountryInfo getCountryInfo() { return countryInfo; }
 
-    public Integer getCases() {
-        return cases;
-    }
+  public Integer getCases() { return cases; }
 
-    public Integer getTodayCases() {
-        return todayCases;
-    }
+  public Integer getTodayCases() { return todayCases; }
 
-    public Integer getDeaths() {
-        return deaths;
-    }
+  public Integer getDeaths() { return deaths; }
 
-    public Integer getTodayDeaths() {
-        return todayDeaths;
-    }
+  public Integer getTodayDeaths() { return todayDeaths; }
 
-    public Integer getRecovered() {
-        return recovered;
-    }
+  public Integer getRecovered() { return recovered; }
 
-    public Integer getActive() {
-        return active;
-    }
+  public Integer getActive() { return active; }
 
-    public Integer getCritical() {
-        return critical;
-    }
+  public Integer getCritical() { return critical; }
 
-    public Integer getCasesPerOneMillion() {
-        return casesPerOneMillion;
-    }
+  public Integer getCasesPerOneMillion() { return casesPerOneMillion; }
 
-    public Integer getDeathsPerOneMillion() {
-        return deathsPerOneMillion;
-    }
+  public Integer getDeathsPerOneMillion() { return deathsPerOneMillion; }
 
-    public Integer getTests() {
-        return tests;
-    }
+  public Integer getTests() { return tests; }
 
-    public Integer getTestsPerOneMillion() {
-        return testsPerOneMillion;
-    }
+  public Integer getTestsPerOneMillion() { return testsPerOneMillion; }
 
-    public String getContinent() {
-        return continent;
-    }
-
+  public String getContinent() { return continent; }
 }
--- app/src/main/java/com/github/crazyuploader/covid19/misc/Format.java
+++ app/src/main/java/com/github/crazyuploader/covid19/misc/Format.java
@@ -7,21 +7,22 @@ import java.util.Locale;
 
 public class Format {
 
-    public static String number(int number){
-        return NumberFormat.getNumberInstance(Locale.US).format(number);
-    }
+  public static String number(int number) {
+    return NumberFormat.getNumberInstance(Locale.US).format(number);
+  }
 
-    public static String number(long number){
-        return NumberFormat.getNumberInstance(Locale.US).format(number);
-    }
+  public static String number(long number) {
+    return NumberFormat.getNumberInstance(Locale.US).format(number);
+  }
 
-    public static String number(double number){
-        return NumberFormat.getNumberInstance(Locale.US).format(number);
-    }
+  public static String number(double number) {
+    return NumberFormat.getNumberInstance(Locale.US).format(number);
+  }
 
-    public static String date(Long input){
-        Date date = new Date(input);
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM dd, yyyy ; HH:mm:ss", Locale.getDefault());
-        return  simpleDateFormat.format(date);
-    }
+  public static String date(Long input) {
+    Date date = new Date(input);
+    SimpleDateFormat simpleDateFormat =
+        new SimpleDateFormat("MMM dd, yyyy ; HH:mm:ss", Locale.getDefault());
+    return simpleDateFormat.format(date);
+  }
 }
