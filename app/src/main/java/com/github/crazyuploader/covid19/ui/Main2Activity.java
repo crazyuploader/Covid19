e754763c1078907d96740705e33c6d61cafdd8ee
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-10-08 09:56:11 +0530

    Add Microsoft App Center

--- app/src/main/java/com/github/crazyuploader/covid19/ui/Main2Activity.java
+++ app/src/main/java/com/github/crazyuploader/covid19/ui/Main2Activity.java
@@ -11,6 +11,9 @@ import com.github.crazyuploader.covid19.ui.frags.IndianStates;
 import com.github.crazyuploader.covid19.ui.frags.MapsOverview;
 import com.github.crazyuploader.covid19.ui.frags.GlobalOverview;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class Main2Activity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -19,6 +22,8 @@ public class Main2Activity extends AppCompatActivity implements BottomNavigation
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        AppCenter.start(getApplication(), "00fd6ff5-6459-43e1-8d46-2a512b249a50",
+                Analytics.class, Crashes.class);
 
         bottomNavigationView = findViewById(R.id.bottom_navigation_view);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
